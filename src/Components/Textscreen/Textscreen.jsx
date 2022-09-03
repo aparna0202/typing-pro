c5238abad35fe6c43066db9fea76c1043b3f2ad9
aparna0202 <aparna.pandey172004@gmail.com>
2022-09-03 21:08:47 +0530

    Added API, input mapping and regex.

--- src/Components/Textscreen/Textscreen.jsx
+++ src/Components/Textscreen/Textscreen.jsx
@@ -1,28 +1,50 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
+import { API_URL } from "../../Common/api";
 
 const Textscreen = () => {
-  const [para, setPara] = useState("");
-  const handleKeyDown = (e) => {
-    para[0].split("").forEach((c) => {
-      if (c === e.key) {
-        console.log("matched");
-      }
-    });
+  const [para, setPara] = useState([]);
+  const [currentText, setCurrentText] = useState("");
+  const regex = new RegExp(
+    "[ A-Za-z0-9!@#$%^&*()-_=+`~;:,.<>/?|\\'\"\\[\\]\\{\\}]"
+  );
+
+  const checkLetter = (e) => {
+    if (!regex.test(e)) return;
+    setCurrentText(e);
+    para[currentText.length].isCorrect =
+      para[currentText.length].letter === e.slice(-1) ? true : false;
+
+    console.log(para[currentText.length].isCorrect);
+  };
+
+  const fetchParagraph = () => {
+    fetch(API_URL)
+      .then((response) => response.text())
+      .then((text) =>
+        setPara(
+          text.split("").map((item) => {
+            return { letter: item, isCorrect: null };
+          })
+        )
+      );
   };
 
   useEffect(() => {
-    fetch(`https://baconipsum.com/api/?type=meat-and-filler&paras=1`).then(
-      (result) => result.json().then((result) => setPara(result))
-    );
+    fetchParagraph();
   }, []);
 
   return (
     <div className="textscreen">
       <div className="display">
-        <p>{para}</p>
+        <p>{para.map((item) => item.letter)}</p>
       </div>
-      <input type="text" className="enteredText" onKeyDown={handleKeyDown} />
+      <input
+        type="text"
+        className="enteredText"
+        onChange={(e) => checkLetter(e.target.value)}
+        value={currentText}
+      />
     </div>
   );
 };
