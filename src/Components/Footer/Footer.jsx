a43dab33da4c232b70b094a198a2abda6a20cdf4
aparna0202 <aparna.pandey172004@gmail.com>
2022-09-26 21:49:22 +0530

    Designed Footer.

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p>Developed By Aparna Pandey</p>
+    </div>
+  );
+};
+
+export default Footer;
