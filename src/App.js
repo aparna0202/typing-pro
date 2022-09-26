d1d57ef14b4f279ded58a7b3e0e6eb8977254f95
aparna0202 <aparna.pandey172004@gmail.com>
2022-09-26 21:48:48 +0530

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Textscreen from "./Components/Textscreen/Textscreen";
 import "./App.css";
-
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import { Route, Routes } from "react-router-dom";
+import HistoryPage from "./Components/HistoryPage/HistoryPage";
 function App() {
   return (
     <div className="App">
-      <Textscreen />
+      <Header />
+      <Routes>
+        <Route path="/home" element={<Textscreen />} />
+        <Route path="/history" element={<HistoryPage />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
